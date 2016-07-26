7997e7fb32e31613a2641ff21ad5b345929d3386
kucats <kucats@sfc.wide.ad.jp>
2016-07-26 15:41:40 +0900

    Delete syntax error

--- parser.py
+++ parser.py
@@ -111,7 +111,7 @@ class Kyoshu(object):
 					if(hours['description']=='Available' or hours['description']=='Available(S)'):
 						self.slack.notify(text='[空き]'+date+' '+str(hours['hour'])+'限の予約ができるようになりました (state has changed from '+saved_hours['description']+' to '+hours['description']+')')
 					elif(hours['description']=='Unavailable'):
-						self.slack.notify(text=+date+' '+str(hours['hour'])+'限は、予約されてしまいました。 (state has changed from '+saved_hours['description']+' to '+hours['description']+')')
+						self.slack.notify(text=date+' '+str(hours['hour'])+'限は、予約されてしまいました。 (state has changed from '+saved_hours['description']+' to '+hours['description']+')')
 					else:
 						self.slack.notify(text='Date: '+date+' Hour:'+str(hours['hour'])+' state has changed from '+saved_hours['description']+' to '+hours['description'])
 
