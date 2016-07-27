b4c87a39a515bc027862a5400b3b46c5b952cb62
kucats <kucats@kuc.jp>
2016-07-27 18:25:39 +0900

    Implement reserve function

--- parser.py
+++ parser.py
@@ -9,6 +9,8 @@ import slackweb
 
 from lxml import html
 
+from urllib.parse import urlparse, parse_qs
+
 import configparser
 import json
 
@@ -57,13 +59,105 @@ class Kyoshu(object):
 			operations.append(b)
 		self.operations=operations
 
-	def get_page_reservation(self):
-		operations=self.operations
+	def _notify(self,text):
+		self.slack.notify(text=text)
 
+	def _filter_operations_by_name(self,name):
+		operations=self.operations
 		for ops in operations:
 			if ops['action_ja'] == '技能予約':
 				url=ops['url']
-		r = self.session_requests.get(self.url_base+url)
+				return self.url_base+url
+		return False
+
+	def _filter_string_by_date(self,string,month,day):
+		if month+'月' in string and day+'日' in string:
+			return True
+
+	def _filter_dom_by_date(self,doms,month,day):
+		for dom in doms:
+			if self._filter_string_by_date(dom.text,month,day):
+				return dom
+		return False
+
+	def do_reserve(self,month,day,hour):
+		#特定の時限の予約を行う
+		g = self.get_reserve_page(month,day)
+		for each in g:
+			each_hour = each.get('hour',{})
+			each_url = each.get('url',{})
+			if hour==each_hour:
+				url=each_url
+				break
+		if url is False:
+			return False
+
+		r = self.session_requests.get(url)
+		r.encoding='Shift_JIS'
+
+		#指名があれば
+
+		#指名がなければ
+		dom = html.fromstring(r.text.strip())
+		form_dom=dom.xpath('//form')[0]
+
+		action = form_dom.action
+
+		input_dom=dom.xpath('//form/input')
+		payload = {}
+
+		#hidden パラメータを読み込んで送るようにする
+		for dom in input_dom:
+			if 'b.' in dom.name or 'token' in dom.name or 'struts.token.name' in dom.name:
+				payload[dom.name] = dom.value
+
+		payload['method:doYes'] = 1
+
+		r = self.session_requests.post(
+			self.url_base+action,
+			data = payload
+		)
+		r.encoding='Shift_JIS'
+		dom = html.fromstring(r.text.strip().replace('BR', '').replace('br', ''))
+		#fetch error
+		font_dom_ok=dom.xpath("//font[@class='ok']")
+		font_dom_error=dom.xpath("//font[@class='error']")
+		if len(font_dom_ok) != 0 and font_dom_ok[0].text is not False:
+			self._notify('予約成功: ('+month+'/'+day+' '+hour+'限) を予約しました')
+		elif len(font_dom_error) != 0 and font_dom_error[0].text is not False:
+			self._notify('予約エラー: ('+month+'/'+day+' '+hour+'限) '+font_dom_error[0].text)
+		else:
+			self._notify('例外発生: ('+month+'/'+day+' '+hour+'限) 予約のステータスを確認してください')
+
+
+
+	def get_reserve_page(self,month,day):
+		#その日にちの予約URL一覧を取得
+		r = self.session_requests.get(self._filter_operations_by_name('技能予約'))
+		r.encoding='Shift_JIS'
+		dom = html.fromstring(r.text.strip())
+		link_doms=dom.xpath('//a')
+		dom = self._filter_dom_by_date(link_doms,month,day)
+
+		#日付ごとの予約ページを取得する
+		r = self.session_requests.get(self.url_base+dom.attrib['href'])
+		r.encoding='Shift_JIS'
+		dom = html.fromstring(r.text.strip())
+		link_doms=dom.xpath('//a')
+		list=[]
+		for dom in link_doms:
+			b={}
+			#時間指定のものを抜く
+			if '○' in dom.text or '×' in dom.text or 'Ｓ' in dom.text:
+				u= parse_qs(urlparse(self.url_base+dom.attrib['href']).query)
+				b['hour']=u.get('b.infoPeriodNumber', [''])[0]
+				b['url']=self.url_base+dom.attrib['href']
+				list.append(b)
+		return list
+
+
+	def get_page_reservation(self):
+		r = self.session_requests.get(self._filter_operations_by_name('技能予約'))
 		r.encoding='Shift_JIS'
 		dom = html.fromstring(r.text.strip())
 		link_doms=dom.xpath('//a')
@@ -109,11 +203,11 @@ class Kyoshu(object):
 			for saved_hours,hours in zip(saved_date_dict['schedule'],date_dict['schedule']):
 				if saved_hours['description'] != hours['description']:
 					if(hours['description']=='Available' or hours['description']=='Available[S]'):
-						self.slack.notify(text='[空き]'+date+' '+str(hours['hour'])+'限の予約ができるようになりました (state has changed from '+saved_hours['description']+' to '+hours['description']+')')
+						self._notify('[空き]'+date+' '+str(hours['hour'])+'限の予約ができるようになりました (state has changed from '+saved_hours['description']+' to '+hours['description']+')')
 					elif(hours['description']=='Unavailable'):
-						self.slack.notify(text=date+' '+str(hours['hour'])+'限は、予約されてしまいました。 (state has changed from '+saved_hours['description']+' to '+hours['description']+')')
+						self._notify(date+' '+str(hours['hour'])+'限は、予約されてしまいました。 (state has changed from '+saved_hours['description']+' to '+hours['description']+')')
 					else:
-						self.slack.notify(text='Date: '+date+' Hour:'+str(hours['hour'])+' state has changed from '+saved_hours['description']+' to '+hours['description'])
+						self._notify('Date: '+date+' Hour:'+str(hours['hour'])+' state has changed from '+saved_hours['description']+' to '+hours['description'])
 
 	def _save_schedule_to_file(self,dict):
 		with open(self.file_schedule_all,'w') as f:
@@ -168,6 +262,7 @@ Kyoshu = Kyoshu()
 
 def main():
 	Kyoshu.get_page_reservation()
+	#Kyoshu.do_reserve('7','29','9')
 
 if __name__ == '__main__':
 	main()
